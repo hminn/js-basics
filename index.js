f549e6219abd28afcbf0e22ce735831eeda4d09d
hminn <hmkim0008@gmail.com>
2019-11-14 19:41:44 +0900

    Day2. plus

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ function handleClick() {
   //   } else {
   //     title.classList.add(CLICKED_CLASS);
   //   }
+  //   위의 코드를 toggle을 이용해 간단하게 표현할 수 있다.
+  //   해당 클래스 안에 CLICKED_CLASS가 없다면 add / 있다면 remove 하는 함수
   title.classList.toggle(CLICKED_CLASS);
 }
 
