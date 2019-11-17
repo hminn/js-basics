5b761aff6008bd18150b5f8b5208268e366b1854
hminn <hmkim0008@gmail.com>
2019-11-17 20:55:45 +0900

    이론 / 실전 파일 분할
    - 이론에서 사용한 파일은 Practice로 변경

---
+++ practice2.js
@@ -0,0 +1,21 @@
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+  const hasClass = title.classList.contains(CLICKED_CLASS);
+  // if (hasClass) {
+  //   title.classList.remove(CLICKED_CLASS);
+  // } else {
+  //   title.classList.add(CLICKED_CLASS);
+  // }
+  //   위의 코드를 toggle을 이용해 간단하게 표현할 수 있다.
+  //   해당 클래스 안에 CLICKED_CLASS가 없다면 add / 있다면 remove 하는 함수
+  title.classList.toggle(CLICKED_CLASS);
+}
+
+function init() {
+  title.addEventListener("click", handleClick);
+}
+
+init();
