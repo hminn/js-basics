33cefd0420fdca3eed109cabd3c96946e9a1cad5
hminn <hmkim0008@gmail.com>
2019-11-18 18:34:45 +0900

    #3.6 Making a To Do List 2
    - 입력한 to do list를 localStorage에 저장하고 새로고침 시에도 list가 보이게끔 작업
    - localStorage에는 자바스크립트의 data를 저장할 수가 없다. 오직 string만 저장이 가능함.
    - 자바스크립트는 local storage에 있는 모든 데이터를 string으로 저장하려고 한다.
    - 이러한 문제를 해결하기위해 JSON.stringify를 사용한다.  이는 자바스크립트의 object를 string으로 바꿔준다.
    - JSON? 'Javascript Object Notation'
    데이터를 전달할 때, 자바스크립트가 그걸 다룰 수 있도록 object로 바꿔주는 기능인 셈.
    - forEach : array의 method로 array 객체 하나하나에 함수를 실행시킨다.

--- todo.js
+++ todo.js
@@ -4,22 +4,40 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
-  }
+const toDos = [];
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.innerText = "❌";
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌";
   span.innerText = text;
 
   li.appendChild(span);
   li.appendChild(delBtn);
+  li.id = newId;
   toDoList.appendChild(li);
+
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo) {
+      paintToDo(toDo.text);
+    });
+  }
 }
 
 function handleSubmit(event) {
