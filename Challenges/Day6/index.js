503db1b9c0b8939fefeeabc2744a75797884ed36
hminn <hmkim0008@gmail.com>
2019-12-09 18:13:20 +0900

    Day6.Code_Challenge(코드 정정)
    - 이전 코드의 문제점 파악
    - 새로고침(refresh)시 select 창에 선택된 country가 남아있도록 하는 부분 개선(load)
    - 이전 코드는 전체적으로 비효율적인 코드

--- Challenges/Day6/index.js
+++ Challenges/Day6/index.js
@@ -2,19 +2,36 @@
 // import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
 const COUNTRY_KEY = "country";
+const select = document.querySelector(".selectBox");
 
-function getCountry() {
-  const selectBox = document.querySelector(".selectBox");
-  const text = selectBox.options[selectBox.selectedIndex].text;
-  if (
-    localStorage.getItem(COUNTRY_KEY) !== text &&
-    text !== "--- Choose Your Country ---"
-  ) {
-    localStorage.setItem(COUNTRY_KEY, text);
+function handleCountry() {
+  const selected = select.value;
+  localStorage.setItem(COUNTRY_KEY, selected);
+}
+
+function loadCountry() {
+  const selected = localStorage.getItem(COUNTRY_KEY);
+  if (selected) {
+    const option = document.querySelector(`option[value=${selected}]`);
+    option.selected = true;
   }
 }
-function init() {
-  getCountry();
-}
 
-init();
+loadCountry();
+select.addEventListener("change", handleCountry);
+
+// function getCountry() {
+//   const selectBox = document.querySelector(".selectBox");
+//   const text = selectBox.options[selectBox.selectedIndex].text;
+//   if (
+//     localStorage.getItem(COUNTRY_KEY) !== text &&
+//     text !== "--- Choose Your Country ---"
+//   ) {
+//     localStorage.setItem(COUNTRY_KEY, text);
+//   }
+// }
+// function init() {
+//   getCountry();
+// }
+
+// init();
